dffc43b2a416a322b86d222928b5571379f6f5b0
axroot <axroot@axroot.local>
2025-03-24 15:31:23 -0300

    Initial commit for GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/axr00t.github.io',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig 
